12e9dbce5c6a87ba08435ad7e8da8034ac1fe968
Denyse <mdenyse15@gmail.com>
2021-11-28 11:05:31 +0200

    Feat:(Successfully added a new font and iamges)

--- onboarding_one/lib/main.dart
+++ onboarding_one/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:onboarding_one/onboarding_one.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'My cool first flutter app',
-      home: Text("Hello flutter world"),
-    );
+    return MaterialApp(
+        title: 'My cool first flutter app',
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("My first flutter app"),
+            ),
+            body: const OnBoardingOne()));
   }
 }
---
+++ onboarding_one/lib/onboarding_one.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingOne extends StatelessWidget {
+  const OnBoardingOne({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("Onboarding One"),
+    );
+  }
+}
